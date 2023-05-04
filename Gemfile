f8cb8137278bb3f34d4f4cb93bf38dd42dfe954e
Maxime Robion <max.robion@hey.com>
2023-05-04 12:20:17 +0200

    adding streaming (create, update, destroy) + redis

--- Gemfile
+++ Gemfile
@@ -34,7 +34,7 @@ gem "compass"
 gem "simple_form", "~> 5.1.0"
 
 # Use Redis adapter to run Action Cable in production
-# gem "redis", "~> 4.0"
+gem "redis", "~> 4.0"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,23 @@ class Quote < ApplicationRecord
     validates :name, presence: true
 
     scope :ordered, -> { order(id: :desc) }
+
+    # Should be executed every time a new quote is inserted into the database.
+    after_create_commit -> { 
+                                # instructs our app that the HTML of the created
+                                # quote should be broadcasted to users subscribed
+                                # to the "quotes" stream and prepended to the DOM
+                                # node with the id of "quotes".
+
+                                broadcast_prepend_to "quotes"
+
+                                # https://www.hotrails.dev/turbo-rails/turbo-streams
+                                # Under the hood, Turbo has a default value for both the partial and the locals option.
+                                # partial: "quotes/quote", 
+                                # locals: { quote: self }, 
+                                # By default, the target option will be equal to model_name.plural, 
+                                # target: "quotes" 
+                            }
+    after_update_commit -> { broadcast_replace_to "quotes" }
+    after_destroy_commit -> { broadcast_remove_to "quotes" }                  
 end
