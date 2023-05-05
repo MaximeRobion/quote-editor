7ebdcd5c2358d36a16f7a824c1cf46505c28fa7d
Maxime Robion <max.robion@hey.com>
2023-05-05 13:12:30 +0200

    broadcasting stream to the right people

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,28 +1,6 @@
 class Quote < ApplicationRecord
     belongs_to :company
     validates :name, presence: true
-
     scope :ordered, -> { order(id: :desc) }
-
-    # Should be executed every time a new quote is inserted into the database.
-    #after_create_commit -> { 
-                                # instructs our app that the HTML of the created
-                                # quote should be broadcasted to users subscribed
-                                # to the "quotes" stream and prepended to the DOM
-                                # node with the id of "quotes".
-
-                                #broadcast_prepend_later_to "quotes"
-
-                                # https://www.hotrails.dev/turbo-rails/turbo-streams
-                                # Under the hood, Turbo has a default value for both the partial and the locals option.
-                                # partial: "quotes/quote", 
-                                # locals: { quote: self }, 
-                                # By default, the target option will be equal to model_name.plural, 
-                                # target: "quotes" 
-                            #}
-    #after_update_commit -> { broadcast_replace_later_to "quotes" }
-    #after_destroy_commit -> { broadcast_remove_to "quotes" }    
-    
-    # Those three callbacks are equivalent to the following single line
-    broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+    broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
