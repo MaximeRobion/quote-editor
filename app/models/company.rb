8d426e747ae7c8c4ac1b7655dcdc65b4cf565ce5
Maxime Robion <max.robion@hey.com>
2023-05-05 11:20:18 +0200

    adding relations company/users/quotes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ApplicationRecord
+    has_many :users, dependent: :destroy
+    has_many :quotes, dependent: :destroy
     validates :name, presence: true
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,5 @@
 class Quote < ApplicationRecord
+    belongs_to :company
     validates :name, presence: true
 
     scope :ordered, -> { order(id: :desc) }
