f8e4ece7a0866d3e3dff77c86d2626b6a224c237
Maxime Robion <max.robion@hey.com>
2023-05-10 18:38:46 +0200

    adding turbostream flash on create/delete/update

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Entry point for the build script in your package.json
 import "./controllers"
 import "@hotwired/turbo-rails"
-
-// The two following lines disable Turbo on the whole application
-Turbo.session.drive = false
