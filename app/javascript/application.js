1d72a28b77079c3eea8c7f3b5c1efcbaa79d3503
Maxime Robion <max.robion@hey.com>
2023-05-10 11:59:24 +0200

    disabling turbo on the whole app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
 import "./controllers"
+
+// The two following lines disable Turbo on the whole application
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
