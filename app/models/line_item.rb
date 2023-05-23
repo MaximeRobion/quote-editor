8271295de75c7751a59228d70249c5d468d0f41d
Maxime Robion <max.robion@hey.com>
2023-05-23 15:45:23 +0200

    model layer of line items

---
+++ app/models/line_item.rb
@@ -0,0 +1,12 @@
+class LineItem < ApplicationRecord
+  belongs_to :line_item_date
+
+
+  validates :name, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :unit_price, presence: true, numericality: { greater_than: 0 }
+
+  delegate :quote, to: :line_item_date 
+  # that way, one can have access more easily to quote object from here
+  # line_item.quote instead of line_item.line_item_date.quote
+end
