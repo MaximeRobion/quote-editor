9f9253a474cfba7645b25f90c29cded0d92823cf
Maxime Robion <max.robion@hey.com>
2023-05-23 20:42:58 +0200

    edit action w/ turbo

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
     before_action :set_quote
     before_action :set_line_item_date
+    before_action :set_line_item, only: [:edit, :update, :destroy]
   
     def new
       @line_item = @line_item_date.line_items.build
@@ -13,6 +14,17 @@ class LineItemsController < ApplicationController
         redirect_to quote_path(@quote), notice: "Item was successfully created."
       else
         render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+    end
+  
+    def update
+      if @line_item.update(line_item_params)
+        redirect_to quote_path(@quote), notice: "Item was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
       end
     end
   
@@ -28,5 +40,9 @@ class LineItemsController < ApplicationController
   
     def set_line_item_date
       @line_item_date = @quote.line_item_dates.find(params[:line_item_date_id])
+    end
+
+    def set_line_item
+        @line_item = @line_item_date.line_items.find(params[:id])
     end
   end
