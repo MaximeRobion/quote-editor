7d4ea760788f5bbba23113f2f6487b23bbcb0fa3
Maxime Robion <max.robion@hey.com>
2023-05-23 20:44:02 +0200

    delete w/ turbo

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,6 +27,11 @@ class LineItemsController < ApplicationController
         render :edit, status: :unprocessable_entity
       end
     end
+
+    def destroy
+        @line_item.destroy
+        redirect_to quote_path(@quote), notice: "Item was successfully destroyed."
+    end
   
     private
   
